f790b049ee7c92cf28875a331c555aa94c13dd4e
betfaif <betfaif@hotmail.com>
2024-12-17 13:07:16 -0300

    funcionando

--- js/carrito.js
+++ js/carrito.js
@@ -1,3 +1,4 @@
+
 function cargarCarrito() { 
     var listaCarrito = document.getElementById('lista-carrito');
     listaCarrito.innerHTML = '';
@@ -6,10 +7,20 @@ function cargarCarrito() {
         var producto = carrito[i];
         var li = document.createElement('li');
         //li.textContent = producto.nombre + ' - $' + producto.precio;
-        li.textContent = producto.cantidad + '  -  '+producto.nombre + ' - $' + producto.precio ;
+        li.textContent = producto.cantidad + '  -  '+producto.nombre + ' - $' + producto.precio + ' - $'+producto.precio*producto.cantidad;
         listaCarrito.appendChild(li);
+        subtotal=subtotal+(producto.precio * producto.cantidad)
     } 
+
 }
 
 
+
+
+let subtotal=0
 cargarCarrito()
+console.log(subtotal)
+const subtotalElement = document.getElementById('subtotal');
+console.log(subtotalElement)
+subtotalElement.textContent = `Subtotal: $${subtotal.toFixed(2)}`;
+
